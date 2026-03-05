18ff874d4c4674a3c6fafa7ef6131d652328c454
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-05 19:40:28 +0700

    update database

--- dist/modules/chat/chat.database.js
+++ dist/modules/chat/chat.database.js
@@ -1,2 +1,25 @@
-export {};
+import { getDb } from "../../database/mongo.js";
+export class ChatDatabase {
+    col() {
+        return getDb().collection("chat_messages");
+    }
+    async insert(doc) {
+        const res = await this.col().insertOne(doc);
+        return {
+            ...doc,
+            _id: res.insertedId,
+        };
+    }
+    // localhost:9999/api/chat/messages?limit=50&before=<time>
+    async list(params) {
+        const filter = {};
+        if (params.before)
+            filter.createdAt = { $lt: params.before };
+        return this.col()
+            .find(filter)
+            .sort({ createdAt: -1, _id: -1 })
+            .limit(params.limit)
+            .toArray();
+    }
+}
 //# sourceMappingURL=chat.database.js.map
