0d5f214316b39b592b118f37596937751211d089
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-05 20:31:24 +0700

    update controller

--- dist/modules/chat/chat.controller.js
+++ dist/modules/chat/chat.controller.js
@@ -1,2 +1,19 @@
-export {};
+import { create } from "domain";
+export class ChatController {
+    chatService;
+    constructor(chatService) {
+        this.chatService = chatService;
+    }
+    // localhost:9999/api/chat/messages?limit=50&before=<time>
+    listMessages = async (req, res) => {
+        const messages = await this.chatService.listHistory(req.query);
+        res.json(messages.map((m) => ({
+            id: m._id.toString(),
+            userEmail: m.userEmail,
+            role: m.role,
+            text: m.text,
+            createdAt: m.createdAt,
+        })));
+    };
+}
 //# sourceMappingURL=chat.controller.js.map
