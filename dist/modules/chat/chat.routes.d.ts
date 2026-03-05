c1a5d2bbc763236cf58ff8efaadad7fb1e7405c1
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-05 20:33:30 +0700

    update

--- dist/modules/chat/chat.routes.d.ts
+++ dist/modules/chat/chat.routes.d.ts
@@ -1,2 +1,2 @@
-export {};
+export declare const chatRouter: import("express-serve-static-core").Router;
 //# sourceMappingURL=chat.routes.d.ts.map
--- dist/modules/chat/chat.routes.js
+++ dist/modules/chat/chat.routes.js
@@ -1,2 +1,11 @@
-export {};
+import { Router } from "express";
+import { ChatDatabase } from "./chat.database.js";
+import ChatService from "./chat.service.js";
+import { ChatController } from "./chat.controller.js";
+const router = Router();
+const db = new ChatDatabase();
+const service = new ChatService(db);
+const controller = new ChatController(service);
+router.get("/messages", controller.listMessages);
+export const chatRouter = router;
 //# sourceMappingURL=chat.routes.js.map
